3994c5c523625a171b6c1f00c9086d3406097aed
wasim akram <akram.wasimsd@gmail.com>
2025-07-16 23:10:44 +0530

    delete backend

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "eks-s3-bucket1"     # 🔁 Replace with your bucket name
-    key            = "eks/terraform.tfstate"          # Path inside the bucket
-    region         = "us-east-1"                      # AWS region
-    encrypt        = true                             # Encrypt state file with SSE
-    dynamodb_table = "terraform-locks"                # (Optional) For state locking
-  }
-}

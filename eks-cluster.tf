5729fd8b7234e8f803436cd1b917d367e41cf461
wasim akram <akram.wasimsd@gmail.com>
2025-07-16 23:40:07 +0530

    wasim

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -15,7 +15,7 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type               = "AL2_x86_64"
-    instance_types         = "g4dn.xlarge"
+    instance_types         = "t3.medium"
     vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
   }
   
